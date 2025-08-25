94e92333fdd8b491a4824ed0de4bf60153859ede
Konjeti Bala Venkata Sujith <konjetisujith@gmail.com>
2025-08-25 18:01:55 +0530

    Remove API rewrites from next.config.js

--- ui/next.config.js
+++ ui/next.config.js
@@ -2,18 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/upload',
-        destination: 'http://localhost:8080/upload',
-      },
-      {
-        source: '/api/download/:port',
-        destination: 'http://localhost:8080/download/:port',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig
